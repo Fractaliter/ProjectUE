dead24035aee10900eb9a19e10518c863e11ca16
Krzysztof Wawrzkiewicz <chris.wawrz@gmail.com>
2025-03-29 14:42:15 +0100

    Small ui changes

--- crm/webapp/admin.py
+++ crm/webapp/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . models import Contact,Event
+from . models import Contact,Event,Project
 
 admin.site.register(Contact)
 admin.site.register(Event)
+admin.site.register(Project)
 
